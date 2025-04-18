4b625a21da60bee3a91d62f779efeb5c6c08adc8
JehyeonLee <jehyeonlee27@gmail.com>
2025-04-17 21:59:28 -0500

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
-import { ThemeProvider as MUIThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
